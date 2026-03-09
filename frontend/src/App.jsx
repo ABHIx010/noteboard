c90096624d6a4d2c85a94abffc99aba777a44d97
ABHIx010 <abhinavlal0280@gmail.com>
2026-03-09 21:09:02 +0530

    content page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/content/:id" element={<Content/>}/>
+        <Route path="/note/:id" element={<Content/>}/>
         <Route path="/create" element={<CreatePage/>}/>
       </Routes>
     </div>
